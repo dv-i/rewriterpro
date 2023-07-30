3be931d7d0eea4a1af4882f7d14b4bf72728f5c1
Jaimish Ashar <jaimish97@gmail.com>
2023-07-30 14:09:34 -0400

    Updates mongo again

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 export const DATABASE = {
 	DATA_SOURCE: "Cluster0",
-	URL: "https://data.mongodb-api.com/app/data-nevnj/endpoint/data/v1",
+	URL: "https://data.mongodb-api.com/app/data-ktijc/endpoint/data/v1",
 	NAME: "rewriterpro",
 	AUTH_URL:
-		"https://realm.mongodb.com/api/client/v2.0/app/data-nevnj/auth/providers/api-key/login",
+		"https://realm.mongodb.com/api/client/v2.0/app/data-ktijc/auth/providers/api-key/login",
 };
 
 export const FEATURE_FLAGS = {
